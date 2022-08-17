bb1e8f03ad0a19f2b0be268d231fe2a1cf83325d
Akos515 <akostamas2005@gmail.com>
2022-08-17 11:27:21 +0200

    2. orai anyag

--- 01_kiiratasok.py
+++ 01_kiiratasok.py
@@ -59,3 +59,90 @@ c = 8
 d = 5
 e = c+d
 print(e)
+
+#kiiratás szöveggel:
+print("Az e változóm értéke: e")
+print("Az e változóm értéke:", e)
+print("Az e változóm értéke: "+str(e))
+print(f"Az e változóm értéke: {e}")
+
+#típuskonverzió
+#ezek a sorok nem egyenlőek a változók típusaival
+int(15)
+str(15)
+float(15)
+
+#műveletek
+
+#összeadás +
+#kivonás -
+#szorzás *
+#hatványozás **
+#osztás maradéka %
+#osztás / vagy //
+
+f = e/3         #maradékos osztás
+print(f)
+
+f = e//3        #egészre kerekített osztás
+print(f)
+
+g = e**2        #hatványozás
+print(g)
+
+h = e%d         #az osztás maradéka
+print(h)
+
+#zárójelek
+i = 5
+i = 3*i+1
+print(i)
+i = 5
+i = 3*(i+1)
+print(i)
+
+#szöveg összefűzése + jel segítségével
+vnev = "Németh"
+knev = "Ákos"
+print(vnev+" "+knev) #a " "-ek között space van, ezt szúrja be
+
+#de ez nem összefűzés
+print(f"{vnev} {knev}")
+
+
+nev = vnev+knev
+print(nev)
+
+nev = f"{vnev} {knev}"      #változóban f string
+print(nev)
+
+#adatok bekérése egy embertől
+#input("Kérek egy keresztnevet: ")
+#print(input("Kérek egy keresztnevet: "))
+
+# valasz = input("Kérek egy nevet: ")
+# print(valasz)
+
+#valaszvnev = input("Kérek egy vezetéknevet: ")
+#valaszknev = input("Kérek egy keresztnevet: ")
+
+#print(f"A kért adat: {valaszvnev} {valaszknev}")
+
+#kérj be két számot és irasd ki az összegüket
+
+# szám1 = int(input("első szám:"))
+# szám2 = int(input("második szám:"))
+# print(f"A két szám összege: {szám1+szám2}")
+
+#lista létrehozása
+lista = []          #üres lista
+print(type(lista))
+print(lista)
+
+lista = [1, 5, 7, 9]
+print(lista)
+
+#lista adott elemének elérése
+lista = ["alma", "körte", "banán"]
+print(lista[0])     #első elem
+print(lista[-1])    #utolsó elem
